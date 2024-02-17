a4df08725ea291a8cb3afdbf44bba70a7b9594a5
Sanjana Thoguru <thogurus@sheridancollege.ca>
2024-02-17 01:02:48 -0500

    Initial Commit

---
+++ SanjanaThoguru991445383/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SanjanaThoguru991445383
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SanjanaThoguru991445383/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SanjanaThoguru991445383
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
